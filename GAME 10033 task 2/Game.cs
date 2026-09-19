5d9595d724118fb3464ec495e34eb5f88ae3738d
Indyz <Indyz@MICHAELFLABTOP>
2026-09-19 08:42:43 -0700

    added hill

---
+++ GAME 10033 task 2/Game.cs
@@ -0,0 +1,46 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+        //tiltle and size of game
+        Window.SetTitle("sunset interactive");
+        Window.SetSize(400, 400);
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+        //background color (sky)
+            Window.ClearBackground(Color.Blue);
+        
+            
+            {
+                //the front hill the sun sets behind
+            Draw.FillColor = Color.Green;
+            Draw.LineSize = 4;
+            Draw.LineColor = Color.Green;
+            Draw.Circle(new Vector2(200, 650), 400);
+            }
+        
+
+        }
+
+    }
+
+}
